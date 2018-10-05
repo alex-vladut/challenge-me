8e283557209928370bf5572e2f6066e187dab2e0
Alexandru Vladut <alexandruvladut@gmail.com>
2018-10-05 19:04:27 +0300

    Removed background color from ViewChallenge component

--- client/challenge-me/src/components/ViewChallenge/ViewChallenge.js
+++ client/challenge-me/src/components/ViewChallenge/ViewChallenge.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import Label from '../../components/UI/Label/Label';
 import Button from '../../components/UI/Button/Button';
@@ -106,6 +107,8 @@ class ViewChallenge extends Component {
                             </div>
                         </div>)
                 }
+
+                <Link to="/challenges/new-challenge" className="BottomRight">+</Link>
             </div>
         );
     }
