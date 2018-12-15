772e95d0a1a976154e34a2f6033064eef595ada4
Alexandru Vladut <alexandruvladut@gmail.com>
2018-12-15 16:18:06 +0200

    Updated CF template to include API Gateway resources

--- client/challenge-me/src/aws-exports.js
+++ client/challenge-me/src/aws-exports.js
@@ -7,7 +7,8 @@ const config = {
         endpoints: [
             {
                 name: "ChallengeMeAPI",
-                endpoint: "https://94uwnllp22.execute-api.eu-central-1.amazonaws.com/production",
+                //endpoint: "https://94uwnllp22.execute-api.eu-central-1.amazonaws.com/production",
+                endpoint: 'https://lsklh0nrd6.execute-api.eu-central-1.amazonaws.com/LATEST',
                 region: 'eu-central-1'
             }
         ]
