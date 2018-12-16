eda9a719eba7da60850712f671ef618fc3a6475d
Alexandru Vladut <alexandruvladut@gmail.com>
2018-12-16 14:06:35 +0200

    Added API Gateway/Lambda config for CreateUser and GetUsers

--- server/challenge-me/src/handler/users_handler.js
+++ server/challenge-me/src/handler/users_handler.js
@@ -12,7 +12,7 @@ const getUser = async (event) => {
     if (user.Item) {
         return {
             statusCode: 200,
-            body: JSON.stringify(user.Item, true, 2),
+            body: JSON.stringify({ ...user.Item, version: undefined }, true, 2),
             headers: {
                 'Access-Control-Allow-Origin': '*'
             }
@@ -27,6 +27,56 @@ const getUser = async (event) => {
     }
 };
 
+const createUser = async (event) => {
+    console.log(JSON.stringify(event));
+    const cognitoIdentityId = event.requestContext.identity.cognitoIdentityId;
+    const profile = JSON.parse(event.body);
+    try {
+        const user = {
+            'id': cognitoIdentityId,
+            'name': profile.name,
+            'coins': 10000,
+            'version': 0
+        }
+        await dynamoDB.put({
+            TableName: 'user',
+            Item: user,
+            ConditionExpression: 'attribute_not_exists(id)'
+        }).promise();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(user),
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        };
+    } catch (error) {
+        console.log(JSON.stringify(error));
+        return {
+            statusCode: 500,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+    }
+};
+
+const getUsers = async (event) => {
+    const response = await dynamoDB.scan({
+        TableName: 'user'
+    }).promise();
+    return {
+        statusCode: 200,
+        body: JSON.stringify(response.Items, true, 2),
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
+    };
+};
+
 module.exports = {
     getUser,
+    createUser,
+    getUsers,
 }
