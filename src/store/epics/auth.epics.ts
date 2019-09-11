1079044206bf4eadc075eb356bcacf090a0fda10
Alex Vladut <alexandruvladut@gmail.com>
2019-09-11 19:24:17 +0300

    Removed creating new user through GraphQL API

--- src/store/epics/auth.epics.ts
+++ src/store/epics/auth.epics.ts
@@ -3,59 +3,28 @@ import { ofType } from "redux-observable";
 import { from, of } from "rxjs";
 import { catchError, map, switchMap } from "rxjs/operators";
 
-import * as mutations from '../../graphql-api/mutations';
 import * as queries from "../../graphql-api/queries";
 import { FetchProfile, FetchProfileFail, FetchProfileNotFound, FetchProfileSuccess, SignOut, SignOutFail, SignOutSuccess } from "../actions/auth.actions";
 
-function fetchProfile(actions$: any) {
-  return actions$.pipe(
-    ofType(FetchProfile.type),
-    switchMap(() => from(Auth.currentAuthenticatedUser())),
-    switchMap((authenticatedUser: any) =>
-      from(API.graphql(graphqlOperation(queries.getUser, { id: authenticatedUser.username }))).pipe(
-        map((response: any) => (response.data.getUser ? FetchProfileSuccess.create(response.data.getUser) : FetchProfileNotFound.create())),
-        catchError(error => of(FetchProfileFail.create(error)))
-      )
+const fetchProfile = (actions$: any) => actions$.pipe(
+  ofType(FetchProfile.type),
+  switchMap(() => from(Auth.currentAuthenticatedUser())),
+  switchMap((authenticatedUser: any) =>
+    from(API.graphql(graphqlOperation(queries.getUser, { id: authenticatedUser.username }))).pipe(
+      map((response: any) => (response.data.getUser ? FetchProfileSuccess.create(response.data.getUser) : FetchProfileNotFound.create())),
+      catchError(error => of(FetchProfileFail.create(error)))
     )
-  );
-}
+  )
+);
 
-function createProfile(actions$: any) {
-  return actions$.pipe(
-    ofType(FetchProfileNotFound.type),
-    switchMap(() => from(Auth.currentAuthenticatedUser())),
-    switchMap((authenticatedUser: any) =>
-      from(
-        API.graphql(
-          graphqlOperation(mutations.createUser, {
-            input: {
-              id: authenticatedUser.username,
-              googleIdentityId: authenticatedUser.username,
-              email: authenticatedUser.attributes.email,
-              name: authenticatedUser.attributes.name,
-              pictureUrl: authenticatedUser.attributes.picture
-            }
-          })
-        )
-      ).pipe(
-        map((response: any) => FetchProfileSuccess.create(response.data.createUser)),
-        catchError(error => of(FetchProfileFail.create(error)))
-      )
+const signOut = (actions$: any) => actions$.pipe(
+  ofType(SignOut.type),
+  switchMap(() =>
+    from(Auth.signOut()).pipe(
+      map(() => SignOutSuccess.create()),
+      catchError(error => of(SignOutFail.create(error)))
     )
-  );
-}
+  )
+);
 
-
-function signOut(actions$: any) {
-  return actions$.pipe(
-    ofType(SignOut.type),
-    switchMap(() =>
-      from(Auth.signOut()).pipe(
-        map(() => SignOutSuccess.create()),
-        catchError(error => of(SignOutFail.create(error)))
-      )
-    )
-  );
-}
-
-export default [fetchProfile, createProfile, signOut];
+export default [fetchProfile, signOut];
