df68b60c5838997b1e0d933bab10180d3a60703f
Alex Vladut <alexandruvladut@gmail.com>
2019-09-13 14:42:22 +0300

    Support multiple redirect URLs

--- src/index.tsx
+++ src/index.tsx
@@ -10,12 +10,47 @@ import { createEpicMiddleware } from "redux-observable";
 
 import Notifications from "./hoc/Notifications";
 import App from "./App";
-import awsExports from "./aws-exports";
+import config from "./aws-exports";
 import registerServiceWorker from "./registerServiceWorker";
 import { rootEpic } from "./store/epics";
 import reducer from "./store/reducers";
 
-Amplify.configure(awsExports);
+const urlsIn = config.oauth.redirectSignIn.split(",");
+const urlsOut = config.oauth.redirectSignOut.split(",");
+const oauth = {
+  domain: config.oauth.domain,
+  scope: config.oauth.scope,
+  redirectSignIn: config.oauth.redirectSignIn,
+  redirectSignOut: config.oauth.redirectSignOut,
+  responseType: config.oauth.responseType
+};
+const hasLocalhost = (hostname: string) => Boolean(hostname.match(/localhost/) || hostname.match(/127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}/));
+const hasHostname = (hostname: string) => Boolean(hostname.includes(window.location.hostname));
+const isLocalhost = hasLocalhost(window.location.hostname);
+if (isLocalhost) {
+  urlsIn.forEach(e => {
+    if (hasLocalhost(e)) {
+      oauth.redirectSignIn = e;
+    }
+  });
+  urlsOut.forEach(e => {
+    if (hasLocalhost(e)) {
+      oauth.redirectSignOut = e;
+    }
+  });
+} else {
+  urlsIn.forEach(e => {
+    if (hasHostname(e)) {
+      oauth.redirectSignIn = e;
+    }
+  });
+  urlsOut.forEach(e => {
+    if (hasHostname(e)) {
+      oauth.redirectSignOut = e;
+    }
+  });
+}
+Amplify.configure({ ...config, oauth });
 
 const epicMiddleware = createEpicMiddleware();
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(epicMiddleware)));
