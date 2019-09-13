948d4846e8a21c4ef5534ea968dc9dbd33fc3dd8
Alex Vladut <alexandruvladut@gmail.com>
2019-09-13 15:36:28 +0300

    Improved signin buttons

--- src/containers/Auth/Auth.tsx
+++ src/containers/Auth/Auth.tsx
@@ -1,7 +1,10 @@
 import React, { FunctionComponent } from "react";
+
+import Auth, { CognitoHostedUIIdentityProvider } from "@aws-amplify/auth";
 import { connect } from "react-redux";
 import { Redirect } from "react-router";
-import Auth, { CognitoHostedUIIdentityProvider } from "@aws-amplify/auth";
+import { FacebookLoginButton, GoogleLoginButton } from "react-social-login-buttons";
+
 import { State } from "../../store/reducers";
 
 interface AuthProps {
@@ -12,14 +15,17 @@ const Authentication: FunctionComponent<AuthProps> = ({ isAuthenticated }) => {
   if (isAuthenticated) {
     return <Redirect to="/" />;
   }
+  const signInWithFacebook = () => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Facebook });
+  const signInWithGoogle = () => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google });
+
   return (
     <div>
-      <button onClick={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Google })}>
-        <p>Sign in with Google</p>
-      </button>
-      <button onClick={() => Auth.federatedSignIn({ provider: CognitoHostedUIIdentityProvider.Facebook })}>
-        <p>Sign in with Facebook</p>
-      </button>
+      <FacebookLoginButton onClick={signInWithFacebook}>
+        <span>Sign in with Facebook</span>
+      </FacebookLoginButton>
+      <GoogleLoginButton onClick={signInWithGoogle}>
+        <span>Sign in with Google</span>
+      </GoogleLoginButton>
     </div>
   );
 };
