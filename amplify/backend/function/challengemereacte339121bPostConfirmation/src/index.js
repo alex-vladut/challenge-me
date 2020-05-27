5140f576e1fe9f3d7f28abbfee22be26c2b655e2
Alex Vladut <alexandruvladut@gmail.com>
2020-05-27 22:12:27 +0300

    chore(app): regenerate auth resources
    + Added depends on step functions for DeleteActivity mutation

--- amplify/backend/function/challengemereacte339121bPostConfirmation/src/index.js
+++ amplify/backend/function/challengemereacte339121bPostConfirmation/src/index.js
@@ -1,7 +1,7 @@
 /*
   this file will loop through all js modules which are uploaded to the lambda resource,
   provided that the file names (without extension) are included in the "MODULES" env variable.
-  "MODULES" is a comma-delimited string.
+  "MODULES" is a comma-delimmited string.
 */
 
 exports.lambdaHandler = async (event, context, callback) => {
